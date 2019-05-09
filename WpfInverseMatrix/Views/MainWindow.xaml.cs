642fbf9f62bb243d94ce8f5425b65e281101187b
Symasoiti97 <symasoiti97@gmail.com>
2019-05-09 16:18:50 +0300

    Init MVVM, added view MainWindow

--- WpfInverseMatrix/Views/MainWindow.xaml.cs
+++ WpfInverseMatrix/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfInverseMatrix
+namespace WpfInverseMatrix.Views
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
